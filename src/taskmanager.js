2f329bfe023afd6c7d161d2a679b734c50037c64
manan_joshi <joshimanan72@gmail.com>
2023-12-20 22:54:06 +0530

    changes

--- src/taskmanager.js
+++ src/taskmanager.js
@@ -1,35 +1,71 @@
 import React, { useState } from 'react'
 
 const taskmanager = () => {
-    
+
     const [tasks, setTasks] = useState([]);
     const [newtask, setnewtask] = useState('');
     const [edittask, setEdittask] = useState('');
     const [editIndex, setEditIndex] = useState(-1);
 
-    const handleAddTask= () => {
-        if(newtask.trim() === '') return;
+    const handleAddTask = () => {
+        if (newtask.trim() === '') return;
         setTasks([...tasks, newTask]);
         setnewtask('');
     };
 
-    const handleEditTask= (index, task) => {
+    const handleEditTask = (index, task) => {
         setEditIndex(index);
         setEdittask(task);
     };
 
     const handleSaveTask = (index) => {
         const updatedTasks = [...tasks];
-        updatedTasks[index]=  editedTask;
+        updatedTasks[index] = editedTask;
         setTasks(updatedTasks);
         setEdittask('');
         setEditIndex(-1);
     };
 
-    const handleDeleteTask =(index) => {
-        const updatedTasks= tasks.filter((task,i) => i!== index);
+    const handleDeleteTask = (index) => {
+        const updatedTasks = tasks.filter((task, i) => i !== index);
         setTasks(updatedTasks);
     };
+
+    return (
+        <div>
+            <h1>Task Manager</h1>
+            <input
+                type='text'
+                value={newtask}
+                onChange={(e) => setnewtask(e.target.value)}
+            />
+            <button onClick={handleAddTask}>Add Task</button>
+            <ul>
+                {tasks.map((task, index) => (
+                    <li key={index}>
+                        {editIndex === index ? (
+                            <input
+                                type="text"
+                                value={editedTask}
+                                onChange={(e) => setEdittask(e.target.value)}
+                            />
+                        ) : (
+                            <span>{task}</span>
+                        )}
+                        {editIndex === index ? (
+                            <button onClick={() => handleSaveTask(index)}>Save</button>
+                        ) : (
+                            <>
+                                <button onClick={() => handleEditTask(index, task)}>Edit</button>
+                                <button onClick={() => handleDeleteTask(index)}>Delete</button>
+                            </>
+                        )}
+                    </li>
+                ))}
+            </ul>
+        </div>
+
+    )
     
 }
 
